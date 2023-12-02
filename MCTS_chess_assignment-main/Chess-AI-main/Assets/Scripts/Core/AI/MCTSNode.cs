afb1791091b90adfd9cb77e21d3b140d0b71ef63
hgandras <hgandras@gmail.com>
2023-12-02 22:03:21 +0100

    added mcts

---
+++ MCTS_chess_assignment-main/Chess-AI-main/Assets/Scripts/Core/AI/MCTSNode.cs
@@ -0,0 +1,60 @@
+﻿namespace Chess
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEditor.Experimental.GraphView;
+    using UnityEngine;
+    using System.Linq;
+
+    /// <summary>
+    /// - A node should contain the parameters of the UCT policy, so it can be calculated. 
+    /// - It should contain the parent node, so the backpropagation can be done
+    /// - Current state, so the model can be moved forward
+    /// - Must have the child nodes, so the next state can be chosen. 
+    ///  
+    /// </summary>
+    public class MCTSNode
+    {
+        private List<MCTSNode> childrenList;
+        private const float C = 1.41f; //Some number, not final
+        public MCTSNode Parent { get; set; }
+        public int Wins { get; set; }
+        public int TimesVisited { get; set; }
+        public Board State { get; set; }
+        public List<MCTSNode> Children { get { return childrenList; } }
+
+        public MCTSNode(MCTSNode parent,Board state)
+        {
+            childrenList= new List<MCTSNode>();
+            Parent = parent;
+            Wins = 0;
+            TimesVisited = 0;
+            State = state;
+        }
+
+        public void AddChild(MCTSNode child)
+        {
+            childrenList.Add(child);
+        }
+
+        /// <summary>
+        /// Returns the child with maximum UCT value
+        /// </summary>
+        /// <returns></returns>
+        public MCTSNode getBestChild()
+        {
+            return childrenList.OrderByDescending(x => x.UCTVal).First(); 
+        }
+
+        //Property, or can be a static method.
+        public float UCTVal{
+            get
+            {
+                float log = Mathf.Log(Parent.TimesVisited) / TimesVisited;
+                return Wins/TimesVisited+C*Mathf.Sqrt(log);
+            }
+        }
+    }
+
+    
+}
---
+++ MCTS_chess_assignment-main/Chess-AI-main/Assets/Scripts/Core/AI/MCTSSearch.cs
@@ -0,0 +1,86 @@
+﻿namespace Chess
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using UnityEngine;
+    using static System.Math;
+    class MCTSSearch : ISearch
+    {
+        public event System.Action<Move> onSearchComplete;
+
+        MoveGenerator moveGenerator;
+
+        Move bestMove;
+        int bestEval;
+        bool abortSearch;
+
+        MCTSSettings settings;
+        Board board;
+        Evaluation evaluation;
+
+        System.Random rand;
+
+        // Diagnostics
+        public SearchDiagnostics Diagnostics { get; set; }
+        System.Diagnostics.Stopwatch searchStopwatch;
+
+        public MCTSSearch(Board board, MCTSSettings settings)
+        {
+            this.board = board;
+            this.settings = settings;
+            evaluation = new Evaluation();
+            moveGenerator = new MoveGenerator();
+            rand = new System.Random();
+        }
+
+        public void StartSearch()
+        {
+            InitDebugInfo();
+
+            // Initialize search settings
+            bestEval = 0;
+            bestMove = Move.InvalidMove;
+
+            moveGenerator.promotionsToGenerate = settings.promotionsToSearch;
+            abortSearch = false;
+            Diagnostics = new SearchDiagnostics();
+
+            SearchMoves();
+
+            onSearchComplete?.Invoke(bestMove);
+
+            if (!settings.useThreading)
+            {
+                LogDebugInfo();
+            }
+        }
+
+        public void EndSearch()
+        {
+            if (settings.useTimeLimit)
+            {
+                abortSearch = true;
+            }
+        }
+
+        void SearchMoves()
+        {
+            // TODO
+            // Don't forget to end the search once the abortSearch parameter gets set to true.
+
+            throw new NotImplementedException();
+        }
+
+        void LogDebugInfo()
+        {
+            // Optional
+        }
+
+        void InitDebugInfo()
+        {
+            searchStopwatch = System.Diagnostics.Stopwatch.StartNew();
+            // Optional
+        }
+    }
+}
